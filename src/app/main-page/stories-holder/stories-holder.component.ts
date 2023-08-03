19c3bb2141ea1bc98144c82ef58d147c9a0bc7a8
michaell027 <miselka12345@gmail.com>
2023-08-03 02:53:25 +0200

    update responsivity

--- src/app/main-page/stories-holder/stories-holder.component.ts
+++ src/app/main-page/stories-holder/stories-holder.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {collection, doc, getDoc, getDocs} from '@angular/fire/firestore';
+import { Observable, from } from 'rxjs';
+import {AngularFirestore, QuerySnapshot} from "@angular/fire/compat/firestore";
+import firebase from "firebase/compat";
+import DocumentData = firebase.firestore.DocumentData;
+
 
 @Component({
   selector: 'app-stories-holder',
@@ -6,7 +12,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./stories-holder.component.css']
 })
 export class StoriesHolderComponent {
-stories = [
+
+
+  stories = [
   {
     title: 'The best of the best',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl sed aliquam ultricies, nunc nisl ultricies nisl, vitae lacinia nisl nisl nec nisl. Sed vitae nisl auctor, aliqu',
