5a64808c6d7839e6a1b148354ea9ee4af4791b6f
michaell027 <miselka12345@gmail.com>
2024-03-17 19:22:28 +0100

    added redirect

--- src/app/main-page/reportage-holder/reportage-holder.component.ts
+++ src/app/main-page/reportage-holder/reportage-holder.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-reportage-holder',
   templateUrl: './reportage-holder.component.html',
-  styleUrls: ['./reportage-holder.component.css']
+  styleUrls: ['./reportage-holder.component.css'],
 })
 export class ReportageHolderComponent {
-
+  redirect() {
+    window.location.href = 'eventsPage';
+  }
 }
